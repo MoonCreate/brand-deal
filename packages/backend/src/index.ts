0b22620cb5e7dc53077c579d534c26d93104e71e
harkon666 <bryan.wicaksanaa@gmail.com>
2025-07-09 16:27:48 +0700

    implement register controller into route

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,10 +1,22 @@
 import { Hono } from "hono";
 import { homeController } from "./domains/home/home.controller";
+import { registerController } from "./domains/register/register.controller";
+import { cors } from 'hono/cors';
+import { PinataSDK } from 'pinata'
+
+interface Bindings {
+  PINATA_JWT: string;
+  GATEWAY_URL: string;
+}
 
 export const config = {
   runtime: "edge",
 };
 
-const app = new Hono().basePath("/api").route("/home", homeController);
+const app = new Hono<{ Bindings: Bindings }>()
+  .use(cors())
+  .basePath("/api")
+  .route("/home", homeController)
+  .route('/register', registerController);
 
 export default app;
