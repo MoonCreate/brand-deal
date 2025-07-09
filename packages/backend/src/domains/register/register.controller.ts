354677bf54e5ac0c4b6c10785f48aba702709a56
harkon666 <bryan.wicaksanaa@gmail.com>
2025-07-09 16:33:14 +0700

    fix types error

--- packages/backend/src/domains/register/register.controller.ts
+++ packages/backend/src/domains/register/register.controller.ts
@@ -2,12 +2,12 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { registerBrandDto } from "./dtos/registerBrand.dto";
 import { registerCreatorDto } from "./dtos/registerCreator.dto";
-import PinataClient from '@pinata/sdk';
+import { PinataSDK } from "pinata";
 import 'dotenv/config';
 
-const pinata = new PinataClient({
-  pinataApiKey: process.env.PINATA_API_KEY,
-  pinataSecretApiKey: process.env.PINATA_SECRET_API_KEY,
+const pinata = new PinataSDK({
+  pinataJwt: process.env.PINATA_JWT!,
+  pinataGateway: "example-gateway.mypinata.cloud",
 });
 
 const registerController = new Hono()
@@ -19,19 +19,11 @@ const registerController = new Hono()
 
       console.log('Received JSON for upload:', jsonData);
 
-      const result = await pinata.pinJSONToIPFS(jsonData, {
-        pinataMetadata: jsonData,
-      });
+      const upload = await pinata.upload.public.json(jsonData);
 
-      console.log('Pinata upload successful:', result);
+      console.log('Pinata upload successful:', upload.cid);
 
-      return c.json({
-        message: 'JSON uploaded to Pinata successfully!',
-        IpfsHash: result.IpfsHash,
-        PinSize: result.PinSize,
-        Timestamp: result.Timestamp,
-        gatewayUrl: `https://gateway.pinata.cloud/ipfs/${result.IpfsHash}`,
-      }, 200);
+      return c.json(upload, 200);
 
     } catch (error: any) {
       console.error('Error uploading JSON to Pinata:', error);
@@ -47,19 +39,11 @@ const registerController = new Hono()
 
       console.log('Received JSON for upload:', jsonData);
 
-      const result = await pinata.pinJSONToIPFS(jsonData, {
-        pinataMetadata: jsonData,
-      });
+      const upload = await pinata.upload.public.json(jsonData);
 
-      console.log('Pinata upload successful:', result);
+      console.log('Pinata upload successful:', upload.cid);
 
-      return c.json({
-        message: 'JSON uploaded to Pinata successfully!',
-        IpfsHash: result.IpfsHash,
-        PinSize: result.PinSize,
-        Timestamp: result.Timestamp,
-        gatewayUrl: `https://gateway.pinata.cloud/ipfs/${result.IpfsHash}`,
-      }, 200);
+      return c.json(upload, 200);
 
     } catch (error: any) {
       console.error('Error uploading JSON to Pinata:', error);
