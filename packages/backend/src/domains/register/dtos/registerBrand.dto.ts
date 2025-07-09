af4767daf2743c99e80e6ecdb83b71c4a82b3dff
harkon666 <bryan.wicaksanaa@gmail.com>
2025-07-09 16:27:12 +0700

    add register brand dto

---
+++ packages/backend/src/domains/register/dtos/registerBrand.dto.ts
@@ -0,0 +1,11 @@
+import z from 'zod';
+
+export const registerBrandDto = z.object({
+  instanceName: z.string(),
+  logoBrand: z.string(),
+  description: z.string(),
+  email: z.string(),
+  web: z.string(),
+  socialLink: z.array(z.object({ type: z.string(), link: z.string() })),
+  nib: z.string(),
+})
