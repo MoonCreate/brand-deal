7b2997f4104d59d6c3d8c97134641434e44ee8a6
harkon666 <bryan.wicaksanaa@gmail.com>
2025-07-16 20:42:09 +0700

    activate ponder

--- packages/backend/src/api/index.ts
+++ packages/backend/src/api/index.ts
@@ -3,9 +3,9 @@ import { homeController } from "./domains/home/home.controller";
 import { registerController } from "./domains/register/register.controller";
 import { campaignController } from "./domains/campaign/campaign.controller";
 import { cors } from 'hono/cors';
-// import { db } from "ponder:api";
-// import schema from "ponder:schema";
-// import { graphql } from "ponder"; 
+import { db } from "ponder:api";
+import schema from "ponder:schema";
+import { graphql } from "ponder"; 
 
 interface Bindings {
   PINATA_JWT: string;
@@ -16,11 +16,11 @@ export const config = {
   runtime: "edge",
 };
 
-// const hql = graphql({ db, schema });
+const hql = graphql({ db, schema });
 
 const app = new Hono<{ Bindings: Bindings }>()
-  // .use(cors())
-  // .use("/graphql", hql)
+  .use(cors())
+  .use("/graphql", hql)
   .basePath("/api")
   .route("/home", homeController)
   .route("/register", registerController)
