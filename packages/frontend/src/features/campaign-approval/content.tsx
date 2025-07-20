37c54b93c18b9b66d17a8b6d78f7e4486b82a5a5
Yuta4u <yudhistira4u@gmail.com>
2025-07-20 14:49:28 +0700

    chore: campaign approval

--- packages/frontend/src/features/campaign-approval/content.tsx
+++ packages/frontend/src/features/campaign-approval/content.tsx
@@ -1,42 +1,91 @@
+import { useNavigate } from '@tanstack/react-router'
+import { Check, Facebook, Instagram, Twitter, X } from 'lucide-react'
 import { Button } from '@/components/buttons/button'
-import { CampaignCard, ProfileCard } from '@/components/cards/card'
-import { SplitPopAnimation } from '@/components/text/split-pop-animation'
+import { CampaignCard } from '@/components/cards/card'
 
 export const CampaignApprovalContent = () => {
+  const nav = useNavigate({ from: '/campaign/approval' })
+
+  const handleBackClick = () => {
+    nav({ to: '/campaign' })
+  }
+
+  const handleCheckClick = () => {
+    nav({ to: '/campaign/approve' })
+  }
+
   return (
-    <div className="w-full min-h-full flex flex-col grow relative my-10 ">
+    <div>
       <div
-        id="campaign-submission-content"
-        className="w-full grow flex relative"
+        id="campaign"
+        className="h-full flex items-center fixed left-0 top-1/2 -translate-y-1/2 w-[32%] bg-black justify-center"
       >
-        <div className="h-full w-[400px] flex items-center">
-          <div className="w-fit mt-14 relative">
-            <div
-              id="campaign-label"
-              className="px-4 w-fit h-14 shadow-pink-300 z-50 absolute -top-6 left-4 -translate-x-1/2 rounded-2xl text-[2.5rem] font-karantina flex items-center justify-center bg-primary text-white shadow-2xl"
-            >
-              Campaign
-            </div>
-            <CampaignCard />
+        <Button className="fixed left-5 top-4" onClick={handleBackClick}>
+          Back
+        </Button>
+        <div className="w-fit mt-14 relative">
+          <div
+            id="campaign-label"
+            className="px-4 w-fit h-14  z-50 absolute -top-6 left-4 -translate-x-1/2 rounded-2xl text-[2.5rem] font-karantina flex items-center justify-center bg-primary text-white"
+          >
+            Campaign
           </div>
+          <CampaignCard />
         </div>
-        <ProfileCard className="flex-1 rounded-3xl overflow-hidden z-10 relative">
-          <div className="font-karantina text-[3rem] text-center mt-10  absolute z-30 right-1/2 translate-x-1/2 text-white bg-black/20 px-3 rounded-2xl ">
-            <SplitPopAnimation text="Campaign" />{' '}
-            <SplitPopAnimation text="Approve" />
-          </div>
-          <div className="absolute w-full z-10 h-full">
-            <img
-              src="https://cdn.80.lv/api/upload/post/5392/images/5d2cf7f6876a8/widen_1220x0.jpg"
-              className="w-full h-full"
-            />
-          </div>
-          <div className="w-full h-12 bottom-0 left-0 bg-black/20 z-40 absolute shadow-2xl"></div>
-          <div className="w-10 h-full bottom-0  bg-secondary z-40 absolute right-0 shadow-2xl "></div>
-          <Button className="absolute bottom-0 right-0 z-40 h-12 w-20 flex justify-center shadow-2xl shadow-black hover:bg-black hover:text-white">
-            APPROVE
-          </Button>
-        </ProfileCard>
+      </div>
+      <div
+        id="campaign-approval"
+        className="w-[68%] fixed right-0 top-0 h-screen border-l bg-primary/80"
+      >
+        <div className="text-8xl text-center bg-black/40 font-karantina py-2 font-bold text-white shadow-lg shadow-white">
+          CAMPAIGN SUBMITTER
+        </div>
+
+        <div className="h-full overflow-y-auto flex flex-col gap-8 pl-10 pr-4 py-4">
+          {new Array(50).fill(0).map((_, i) => (
+            <div
+              key={i}
+              className="w-full min-h-80 bg-black/20 flex relative rounded-r-2xl rounded-l-full p-4 gap-6"
+            >
+              <div className="min-w-80 w-80 h-full rounded-full overflow-hidden relative">
+                <img
+                  src="https://deadline.com/wp-content/uploads/2023/03/Keanu-Reeves-john-wick-4.jpg"
+                  className="w-full h-full z-30"
+                />
+                <div className="z-40 bg-green-30 w-full h-auto flex gap-4 py-3 bottom-0 bg-black/20 absolute  items-center justify-center">
+                  <Facebook className="text-white hover:cursor-pointer" />
+                  <Twitter className="text-white hover:cursor-pointer" />
+                  <Instagram className="text-white hover:cursor-pointer" />
+                </div>
+              </div>
+              <div className="flex flex-col mt-6">
+                <div className="text-8xl font-karantina font-bold text-white max-w-[40ch]">
+                  BRYAN DEWA WICAKSANA ASDASDASD
+                </div>
+                <div className="text-2xl  text-white">
+                  bryandewawicaksana@gmail.com
+                </div>
+              </div>
+              <div className="flex-1 h-full  flex gap-1 pb-7">
+                <Button onClick={handleBackClick} className="w-1/2 bg-primary">
+                  <X size={40} className="mx-auto" />
+                </Button>
+                <Button
+                  onClick={handleCheckClick}
+                  className="w-1/2 bg-secondary hover:bg-secondary/80"
+                >
+                  <Check size={40} className="mx-auto" />
+                </Button>
+              </div>
+              {/* footer  */}
+              <div className={'flex gap-2 absolute z-20 bottom-4 right-4'}>
+                <div className="size-3 rounded-full bg-primary"></div>
+                <div className="size-3 rounded-full bg-secondary"></div>
+                <div className="size-3 rounded-full bg-secondary"></div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
