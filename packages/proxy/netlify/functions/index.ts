c8faca58357920c66c216ffdfabaa45d793cc0ed
Rakemoon <rakemoonkuzmin@gmail.com>
2025-07-25 21:55:33 +0700

    chore: add necessary

--- packages/proxy/netlify/functions/index.ts
+++ packages/proxy/netlify/functions/index.ts
@@ -1,6 +1,8 @@
+import type { Context } from "@netlify/functions";
+
 const endpoint = "http://139.180.147.183:42069";
 
-async function proxy(request: Request) {
+async function proxy(request: Request, context: Context) {
   const url = new URL(request.url);
   const response = await fetch(endpoint + url.pathname);
   return response;
