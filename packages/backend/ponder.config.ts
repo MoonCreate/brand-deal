709fdba77bc8373d5d6fb2b496bb8323a6cd6121
harkon666 <bryan.wicaksanaa@gmail.com>
2025-08-03 20:14:52 +0700

    change chain to etherlink

--- packages/backend/ponder.config.ts
+++ packages/backend/ponder.config.ts
@@ -6,48 +6,41 @@ import { BrandDealABI } from "./abis/BrandDealABI";
 
 export default createConfig({
   chains: {
-    liskTestnet: {
-      id: 4202,
-      rpc: "https://rpc.sepolia-api.lisk.com",
+    etherlinkTestnet: {
+      id: 128123,
+      rpc: "https://node.ghostnet.etherlink.com",
       disableCache: false,
     },
   },
 
   contracts: {
     Brand: {
-      chain: "liskTestnet",
+      chain: "etherlinkTestnet",
       abi: BrandABI,
       address: process.env.BRAND_ADDRESS as `0x${string}`,
-      startBlock: 24391095,
+      startBlock: 20872347,
       // endBlock: 24459809,
     },
     Creator: {
-      chain: "liskTestnet",
+      chain: "etherlinkTestnet",
       abi: CreatorABI,
       address: process.env.CREATOR_ADDRESS as `0x${string}`,
-      startBlock: 24391095,
+      startBlock: 20872347,
       // endBlock: 24459809,
     },
     Campaign: {
-      chain: "liskTestnet",
+      chain: "etherlinkTestnet",
       abi: CampaignABI,
-      startBlock: 24391095,
+      startBlock: 20872347,
       // endBlock: 24459809,
       address: process.env.CAMPAIGN_ADDRESS as `0x${string}`
     },
     BrandDeal: {
-      chain: "liskTestnet",
+      chain: "etherlinkTestnet",
       abi: BrandDealABI,
-      startBlock: 24391095,
+      startBlock: 20872347,
       // endBlock: 24459809,
       address: process.env.BRAND_DEAL_ADDRESS as `0x${string}`
-    },
-  },
-  blocks: {
-    ChainlinkOracleUpdate: {
-      chain: "liskTestnet",
-      interval: 10, // Every 10 blocks
-      startBlock: 24391095,
     },
   },
 });
