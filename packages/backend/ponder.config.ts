c386cf4299f2eacb54a97ac676f2dd4a5eebbdd0
harkon666 <bryan.wicaksanaa@gmail.com>
2025-08-04 14:13:30 +0700

    push

--- packages/backend/ponder.config.ts
+++ packages/backend/ponder.config.ts
@@ -1,48 +1,30 @@
-import { createConfig } from "ponder";
-import { CreatorABI } from "./abis/CreatorABI";
-import { CampaignABI } from "./abis/CampaignABI";
-import { BrandABI } from "./abis/BrandABI";
+import { createConfig, rateLimit } from "ponder";
 import { BrandDealABI } from "./abis/BrandDealABI";
+import { http } from "viem";
 
 export default createConfig({
   chains: {
     etherlinkTestnet: {
       id: 128123,
-      rpc: "https://128123.rpc.thirdweb.com/3c293f82e6e77f130220ed84ebce68cf",
+      rpc: "https://rpc.ankr.com/etherlink_testnet/a5512d45c31850151736d5ebc1db43b5c470628bc02f49fe28e34c8aeedc4f30",
+      ws: "wss://rpc.ankr.com/etherlink_testnet/ws/a5512d45c31850151736d5ebc1db43b5c470628bc02f49fe28e34c8aeedc4f30",
       disableCache: false,
-      pollingInterval: 10000,
-      maxRequestsPerSecond: 50,
     },
   },
 
   contracts: {
-    Brand: {
-      chain: "etherlinkTestnet",
-      abi: BrandABI,
-      address: process.env.BRAND_ADDRESS as `0x${string}`,
-      startBlock: 20872347,
-      // endBlock: 24459809,
-    },
-    Creator: {
-      chain: "etherlinkTestnet",
-      abi: CreatorABI,
-      address: process.env.CREATOR_ADDRESS as `0x${string}`,
-      startBlock: 20872347,
-      // endBlock: 24459809,
-    },
-    Campaign: {
-      chain: "etherlinkTestnet",
-      abi: CampaignABI,
-      startBlock: 20872347,
-      // endBlock: 24459809,
-      address: process.env.CAMPAIGN_ADDRESS as `0x${string}`
-    },
     BrandDeal: {
       chain: "etherlinkTestnet",
       abi: BrandDealABI,
       startBlock: 20872347,
-      // endBlock: 24459809,
-      address: process.env.BRAND_DEAL_ADDRESS as `0x${string}`
+      address: process.env.BRAND_DEAL_ADDRESS as `0x${string}`,
+    },
+  },
+  blocks: {
+    BrandDealIndexer: {
+      chain: "etherlinkTestnet",
+      interval: 100,         // Set chunk ≤ 100 blok
+      startBlock: 20872347, // sama dengan startBlock target Anda
     },
   },
 });
